a79ade689c99a6840f6dfaf2c57c5583e1ef235e
ProfHugo <profhugo7@gmail.com>
2020-10-28 19:51:58 -0700

    Initial commit.

---
+++ src/main/java/edu/team10/lifetime/backend/ITrigger.java
@@ -0,0 +1,22 @@
+package edu.team10.lifetime.backend;
+
+public interface ITrigger {
+	
+	/**
+	 * Check if the condition to fire this trigger is true.
+	 * 
+	 * @return If this is true, the trigger should fire.
+	 */
+	public boolean checkForCondition();
+	
+	/**
+	 * @return The task attached to this trigger.
+	 */
+	public Task getAttachedTask();
+	
+	/**
+	 * @param attachedTask The new task to attach this trigger to.
+	 */
+	public void setAttachedTask(Task attachedTask);
+	
+}
---
+++ src/main/java/edu/team10/lifetime/backend/TimeTrigger.java
@@ -0,0 +1,29 @@
+package edu.team10.lifetime.backend;
+
+import java.time.OffsetTime;
+
+public class TimeTrigger implements ITrigger {
+
+	private Task attachedTask;
+	private OffsetTime timeToTrigger;
+
+	@Override
+	public boolean checkForCondition() {
+		if (OffsetTime.now().equals(timeToTrigger)) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public Task getAttachedTask() {
+		return attachedTask;
+	}
+
+	@Override
+	public void setAttachedTask(Task attachedTask) {
+		this.attachedTask = attachedTask;
+	}
+
+}
