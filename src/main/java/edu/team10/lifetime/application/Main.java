1588281a4384776547fece7f2da4816a16150cd7
melanieChan <53189099+melanieChan@users.noreply.github.com>
2020-11-10 01:51:31 -0800

    added images and more styling

--- src/main/java/edu/team10/lifetime/application/Main.java
+++ src/main/java/edu/team10/lifetime/application/Main.java
@@ -1,7 +1,11 @@
 package edu.team10.lifetime.application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,16 +15,43 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
+			
+			VBox vbox = addSidePanel();
+			root.setLeft(vbox);
 
 			VBox taskDb = new TaskDashboard();
 			root.setCenter(taskDb);
-			Scene scene = new Scene(root, 400, 400);
+			Scene scene = new Scene(root, 600, 400);
 			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private VBox addSidePanel() {
+		VBox vbox = new VBox();
+		vbox.setPadding(new Insets(15, 12, 15, 12));
+		vbox.setStyle("-fx-background-color: DARKSEAGREEN;");
+		
+	    Button taskBtn = new Button("Tasks");
+	    taskBtn.setPrefSize(100, 40);		// adds padding, args: width, height
+	    taskBtn.setStyle("-fx-background-color: transparent;");
+	    
+	    Button settingsBtn = new Button("Settings");
+	    settingsBtn.setPrefSize(100, 40);		// adds padding, args: width, height
+	    settingsBtn.setStyle("-fx-background-color: transparent;");
+	    
+	    Image img = new Image("/images/play.png");
+	    ImageView view = new ImageView(img);
+	    view.setFitHeight(20);
+	    view.setFitWidth(20);
+	    settingsBtn.setGraphic(view);
+	    
+	    vbox.getChildren().addAll(taskBtn, settingsBtn);
+		
+		return vbox;
 	}
 
 	public static void main(String[] args) {
--- src/main/java/edu/team10/lifetime/application/TaskDashboard.java
+++ src/main/java/edu/team10/lifetime/application/TaskDashboard.java
@@ -3,15 +3,19 @@ package edu.team10.lifetime.application;
 import java.util.Optional;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 public class TaskDashboard extends VBox{
 	
@@ -19,15 +23,21 @@ public class TaskDashboard extends VBox{
 		super();
 		this.setPadding(new Insets(15, 12, 15, 12));
 		this.setStyle("-fx-background-color: GHOSTWHITE;");
+		
 
 	    makeAddBtn();
 
 	}
 	
 	public void makeAddBtn() {
+		Image img = new Image("/images/new_task.png");
+	    ImageView view = new ImageView(img);
+		
 		// make & style button
-	    Button addBtn = new Button("new task");
+	    Button addBtn = new Button();
 	    addBtn.setPrefSize(150, 50);		// adds padding, args: width, height
+	    addBtn.setGraphic(view);
+	    addBtn.setStyle("-fx-focus-color: transparent; -fx-faint-focus-color: transparent;");
 	    
 	    // event handler: if button is clicked, window pops up  
 	    addBtn.setOnAction( event -> 
@@ -56,26 +66,45 @@ public class TaskDashboard extends VBox{
 	}
 	public void displayTask(String taskName) {
 		HBox hbox = new HBox();
+		Button task = new Button();
 		
 		// make & style labels & buttons
 		Label name = new Label(taskName);
 		name.setFont(new Font("Arial", 28));	// bigger font than default
 		
-		Label status = new Label("Status: ");
-		status.setFont(new Font("Arial", 20));	
 		
-	    Button playBtn = new Button("play");
+		Label status = new Label("Status: ");
+		status.setFont(new Font("Arial", 20));
+		
+	    Button playBtn = new Button();
 	    playBtn.setPrefSize(50, 50);		// adds padding, args: width, height
+	    
+	    Image img = new Image("/images/play.png");
+	    ImageView view = new ImageView(img);
+	    view.setFitHeight(50);
+	    view.setFitWidth(50);
+	    playBtn.setGraphic(view);
+	    playBtn.setStyle("-fx-background-color: transparent;");
+	    
+	    Image pause = new Image("/images/pause.png");
+	    ImageView pauseView = new ImageView(pause);
+	    pauseView.setFitHeight(50);
+	    pauseView.setFitWidth(50);
 
 	    // event handler
-	    playBtn.setOnAction( event -> 
-		status.setText("Status: timer on"));
+	    playBtn.setOnAction( event -> {
+	    	status.setText("Status: timer on");
+			playBtn.setGraphic(pauseView);
+	    }
+		);
 	    
-	    hbox.getChildren().addAll(name, playBtn, status);
-	    hbox.setSpacing(10);
+	    hbox.getChildren().addAll(playBtn, status, name);
+	    hbox.setSpacing(50);
+	    hbox.setPadding(new Insets(15, 10, 15, 10));
 	    hbox.setBackground(new Background(new BackgroundFill(Color.DARKSEAGREEN,null,null)));
 	    
 	    // add labels & buttons to VBox
+	    //this.getChildren().addAll(task);
 	    this.getChildren().addAll(hbox);
 	}
 
