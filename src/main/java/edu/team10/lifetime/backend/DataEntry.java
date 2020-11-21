d18cf4a595bb4bbc0ea3b73e4f6f566f9919ddb8
Hugo Wong <hugothepvpmaster0722@gmail.com>
2020-11-20 19:41:41 -0800

    First version of DataEntry and DataRecord

--- src/main/java/edu/team10/lifetime/backend/DataEntry.java
+++ src/main/java/edu/team10/lifetime/backend/DataEntry.java
@@ -1,5 +1,88 @@
 package edu.team10.lifetime.backend;
 
-public class DataEntry {
+import java.time.Duration;
+import java.time.LocalDate;
 
+/**
+ * This class represents a single entry in the user's task history. One such
+ * entry includes the task name, the time at which the task was started and
+ * finished, and the amount of time it took to complete this task.
+ * 
+ * @author Hugo Wong
+ *
+ */
+public class DataEntry {
+	
+	private String taskName;
+	
+	private LocalDate startTime, endTime;
+	
+	private Duration elapsedTime;
+
+	public DataEntry(String taskName, LocalDate startTime, LocalDate endTime, Duration elapsedTime) {
+		this.taskName = taskName;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.elapsedTime = elapsedTime;
+	}
+
+	/**
+	 * @return the taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/**
+	 * @param taskName the taskName to set
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	/**
+	 * @return the startTime
+	 */
+	public LocalDate getStartTime() {
+		return startTime;
+	}
+
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(LocalDate startTime) {
+		this.startTime = startTime;
+	}
+
+	/**
+	 * @return the endTime
+	 */
+	public LocalDate getEndTime() {
+		return endTime;
+	}
+
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(LocalDate endTime) {
+		this.endTime = endTime;
+	}
+
+	/**
+	 * @return the elapsedTime
+	 */
+	public Duration getElapsedTime() {
+		return elapsedTime;
+	}
+
+	/**
+	 * @param elapsedTime the elapsedTime to set
+	 */
+	public void setElapsedTime(Duration elapsedTime) {
+		this.elapsedTime = elapsedTime;
+	}
+	
+	
+	
+	
 }
--- src/main/java/edu/team10/lifetime/backend/DataRecord.java
+++ src/main/java/edu/team10/lifetime/backend/DataRecord.java
@@ -1,5 +1,74 @@
 package edu.team10.lifetime.backend;
 
+import java.util.LinkedList;
+
+/**
+ * This class represents the user's task history as a collection of data
+ * entries.
+ * 
+ * @author Hugo Wong
+ *
+ */
 public class DataRecord {
+
+	LinkedList<DataEntry> taskHistory;
+
+	public DataRecord() {
+		taskHistory = new LinkedList<>();
+	}
+
+	/**
+	 *
+	 * @param entry The data entry to add.
+	 * @return Whether or not the entry was successfully added.
+	 */
+	public boolean addToRecord(DataEntry entry) {
+		return taskHistory.add(entry);
+	}
+	
+	/**
+	 *
+	 * @param entry The data entry to remove.
+	 * @return Whether or not the entry was successfully removed.
+	 */
+	public boolean removeFromRecord(DataEntry entry) {
+		return taskHistory.remove(entry);
+	}
+	
+	/**
+	 * Purge a task from the record by removing all data entires related to the task.
+	 * 
+	 * @return Whether or not this operation is successful.
+	 */
+	public boolean purgeTaskFromRecord(String taskName) {
+		return taskHistory.removeAll(this.getTaskHistory(taskName));
+	}
+	 
+	/**
+	 * Wipe the record clean.
+	 * 
+	 * @return Whether or not this operation is successful.
+	 */
+	public boolean clearRecord() {
+		taskHistory = new LinkedList<>();
+		return taskHistory.size() == 0;
+	}
+
+	/**
+	 * 
+	 * @param taskName The name of the task to search for.
+	 * @return A linked list containing the data entry history for the given task.
+	 */
+	public LinkedList<DataEntry> getTaskHistory(String taskName) {
+		LinkedList<DataEntry> history = new LinkedList<>();
+		
+		for (DataEntry entry : taskHistory) {
+			if (entry.getTaskName().equals(taskName)) {
+				history.add(entry);
+			}
+		}
+		
+		return history;
+	}
 
 }
